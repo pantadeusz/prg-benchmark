31430f0139f6dc49f9129a43b063c36661d1ab79
pantadeusz <pantadeusz@lab110l-01>
2018-11-26 11:17:10 +0100

    benchmark bez opt

--- test.cpp
+++ test.cpp
@@ -3,34 +3,76 @@
 #include "s19616/IsThatRealPrime.hpp"
 #include "s19376/zd.hpp"
 #include <iostream>
+#include <iomanip>
+#include <string>
+#include <map>
+#include <random>
+#include <cmath>
+
+#include <iostream>
 #include <chrono>
+#include <functional>
+#include <vector>
 
 // szkielet funkcji main z s18505
 int main()
 {
-    using namespace std;
+	using namespace std;
 	unsigned long int x;
-
-	cout << "Enter a number: ";
-	cin >> x;
-	
-	auto start = chrono::system_clock::now();
-
-    s19376::is_prime(x);
-    s19616::is_prime(x);
-    s18563::isPrime(x);
-	if (s18505::is_prime(x) == false)
+	struct benchmark_s
 	{
-		cout << endl << "Your number is not a prime number" << endl;
-	}
-	else
+		double summary_time;
+		std::string name;
+		std::function<bool(long int)> f;
+	};
+	std::vector<benchmark_s> tests = {
+		{.summary_time = 0.0,
+		 .name = "s19376",
+		 .f = [](long int x) { return s19376::is_prime(x); }},
+		{.summary_time = 0.0,
+		 .name = "s19616",
+		 .f = [](long int x) { return s19616::is_prime(x); }},
+		{.summary_time = 0.0,
+		 .name = "s18563",
+		 .f = [](long int x) { return s18563::isPrime(x); }},
+		{.summary_time = 0.0,
+		 .name = "s18505",
+		 .f = [](long int x) { return s18505::is_prime(x); }}};
+
+	std::random_device r;
+	std::default_random_engine e1(r());
+	std::uniform_int_distribution<int> uniform_dist(1000000, 9999999);
+
+	for (int i = 0; i < 1000; i++)
 	{
-		cout << endl << "Your number is a prime number" << endl;
+		long int x = uniform_dist(e1);
+		std::vector<bool> voting;
+		for (auto &e : tests)
+		{
+			auto start = chrono::system_clock::now();
+			bool result = e.f(x);
+			auto end = chrono::system_clock::now();
+			voting.push_back(result);
+			chrono::duration<double> diff = end - start;
+			e.summary_time += diff.count();
+		}
+		// weryfikacja wynikow
+		bool r = voting.back();
+		for (auto v : voting)
+		{
+			if (v != r)
+			{
+				for (auto vote : voting)
+				{
+					std::cout << " v:" << (vote ? "T" : "N");
+				}
+				std::cout << "  FOR: " << x << std::endl;
+				break;
+			}
+		}
 	}
-
-	auto end = chrono::system_clock::now();
-	chrono::duration<double> diff = end - start;
-
-	cout << "The program checked your number in " << diff.count() << " seconds." << endl;
-	
+	for (auto &e: tests) {
+		std::cout << "Engine:" << e.name << ": " << e.summary_time << std::endl;
+	}
+	return 0;
 }
